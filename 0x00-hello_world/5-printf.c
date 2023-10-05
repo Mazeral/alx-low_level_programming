b595567c80aaa0cf465660deaf86542946be050e
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-05 16:52:05 +0200

    Learning c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - The entry point
+ * Return: Always success (0)
+ */
+
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
