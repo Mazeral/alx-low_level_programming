d0309a393f2ed74e480c200ce5f3e38c85ac8141
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-05 17:20:41 +0200

    Learning c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,11 +17,11 @@ int d = sizeof(y);
 int e = sizeof(z);
 int f = sizeof(a);
 int g = sizeof(b);
-printf("%s, %d", "Size of a char: %d byte(s)\n", c);
-printf("%s, %d", "Size of a int: %d byte(s)\n", d);
-printf("%s, %d", "Size of a long int: %d byte(s)\n", e);
-printf("%s, %d", "Size of a long long int: %d byte(s)\n, a", f);
-printf("%s, %d", "Size of a float: %d byte(s)\n", g);
+printf("Size of a char: %d byte(s)\n", c);
+printf("Size of a int: %d byte(s)\n", d);
+printf("Size of a long int: %d byte(s)\n", e);
+printf("Size of a long long int: %d byte(s)\n, a", f);
+printf("Size of a float: %d byte(s)\n", g);
 
 return (0);
 }
