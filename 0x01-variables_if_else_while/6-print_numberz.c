522b435d0031f6ff3c0995f150464468aa312914
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-06 14:11:42 +0200

    Variables, if else and loops with c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - The entry point
+ * Return: Always success (0)
+ */
+int main(void)
+{
+int i;
+char n = 49;
+for (i = 0; i < 10; i++)
+{
+putchar (n);
+n++;
+}
+return (0);
+}
