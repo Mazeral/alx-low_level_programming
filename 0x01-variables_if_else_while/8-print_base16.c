8a1b17ea4009956c970fb4325ad71f8d35657e87
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-06 14:34:23 +0200

    Variables, if else and loops with c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - The entry point
+ * Return: always success
+ */
+
+int main (void)
+{
+int n = 48;
+char alph = 'a';
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(n);
+n++
+}
+for (i = 0; i < 6; i++)
+{
+putchar(alph);
+alph++;
+}
+putchat('\n');
+return (0);
+}
