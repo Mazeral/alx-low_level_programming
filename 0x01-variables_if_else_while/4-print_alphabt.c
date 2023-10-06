6b5096c6ec4e1c2d96c62f983969e97837af4c3b
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-06 13:55:04 +0200

    Variables, if else and loops with c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - The entry point
+ * Return: always success
+ */
+
+int main(void)
+{
+char sml = 'a';
+for (i = 0; i < 26; i++)
+{
+if (sml != 'e' || sml != 'q')
+putchar(sml);
+sml++;
+}
+return(0);
+}
