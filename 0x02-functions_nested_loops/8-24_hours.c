30587eb2fea1078ce59b641a22a4c4e51a0dc4ea
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-10 10:21:18 +0200

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * jack_bauer - a nested loop function
+ * @void: Nothing worth mentioning
+ * Return: Void
+ */
+void jack_bauer(void)
+{
+char zeros[] = "00"
+int i, j, k, l;
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 10; j++)
+{
+for (k = 0; k < 6; k++)
+{
+for (l = 0; l < 10; l++)
+{
+putchar(i);
+putchar(j);
+putchar(':');
+putchar(k);
+putchar(l);
+putchar('\n');
+}
+}
+}
+}
+}
