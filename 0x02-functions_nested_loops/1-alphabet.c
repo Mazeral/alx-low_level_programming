959df0562b393a6480d0b7258d579d148d1522f4
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-10 09:32:43 +0200

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - the entry point
+ * Return: success (0)
+ */
+
+int main(void)
+{
+char alph[] = "abcdefghijkhlmnopqrstuvwxyz\n";
+_putchar(alph);
+return (0);
+}
