ef4fde361a6c722d7021d1d05894630cce2d5c6e
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-10 09:53:41 +0200

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * print_sign - the solution function
+ * @n: The paramter of the number to be examined
+ * Return: either (1), (0), or (-1)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+return 1;
+if (n == 0)
+return 0;
+if (n < 0)
+return -1;
+}
