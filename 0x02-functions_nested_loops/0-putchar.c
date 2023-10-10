cea40e3d1bf61f714d6b709221cd6f0a42bc6284
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-10 09:27:39 +0200

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * sol - the solution function
+ * Return: returns void
+ *
+ */
+void sol(void)
+{
+char ans[] = "_putchar\n";
+putchar(ans);
+}
+
+/**
+ * main - the entry point
+ * Return: returns success (0)
+ */
+
+int main(void)
+{
+sol();
+return (0);
+}
