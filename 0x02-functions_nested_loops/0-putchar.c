1d69c97ad79236b9ffd12cf02a2b33bbed0939d3
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-10 16:31:07 +0200

    Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,20 @@
 #include"main.h"
 #include<stdio.h>
+#include<string.h>
+/**
+ * my_print - a custom function for printing easily
+ * @t: the parameter that recieves the char array
+ * Return: Nothing
+ */
+void my_print(char t[])
+{
+int i;
+int len = strlen(t);
+for (i = 0; i < len; i++)
+{
+putchar(t[i]);
+}
+}
 /**
  * main - the solution function
  * Return: returns void
