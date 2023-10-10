4c297bed370525949a3620affd67649ca4bcc03f
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-10 10:06:42 +0200

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * _abs - the function of the solution
+ * @int: the input
+ * Return: absolute value of the input
+ */
+int _abs(int x)
+{
+int n = (x**2)**0.5;
+return (0);
+}
