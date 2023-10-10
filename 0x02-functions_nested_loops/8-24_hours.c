65a8d46494ea62578bba336856506db6047d941a
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-10 23:58:32 +0200

    Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,23 +22,17 @@ putchar(t[i]);
  */
 void jack_bauer(void)
 {
-int i, j, k, l;
-for (i = 0; i < 3; i++)
+int min, hour;
+for (hour = 0; hour <= 23; hour++)
 {
-for (j = 0; j < 4; j++)
+for (min = 0; min <= 59; min++)
 {
-for (k = 0; k < 6; k++)
-{
-for (l = 0; l < 10; l++)
-{
-_putchar(i + '0');
-_putchar(j + '0');
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
 _putchar(':');
-_putchar(k + '0');
-_putchar(l + '0');
-_putchar('\n');
-}
-}
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar(10);
 }
 }
 }
