7d3b204fa6426f353420c12b5161227f476825bf
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-10 09:38:50 +0200

    Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /**
  * print_alphabet - the solution function
- * Return: void, nothing*/
+ * Return: void, nothing
+ */
 void print_alphabet(void)
 {
 char alph[] = "abcdefghijkhlmnopqrstuvwxyz\n";
