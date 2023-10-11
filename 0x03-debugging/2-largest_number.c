54614fc2cdd6674e38b71cced4e792fa0260c4eb
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-11 18:38:54 +0200

    Debugging with ALX c course

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,25 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a >= b && b >= c)
+if(a >= b)
+{
+if(a > c)
 {
 largest = a;
 }
-else if (b >= a && a >= c)
+}
+
+else if(b >= a)
+{
+if(b > c)
 {
 largest = b;
 }
-else
-{
-largest = c;
 }
 
+else 
+{
+largest  = c;
+}
 return (largest);
 }
