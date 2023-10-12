d71a2a2fb530f35df641534aacd0ed529cc1d9cb
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-12 09:18:17 +0200

    ALX nested loops and function part 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - a function that return the multipilication of 2 integers
+ * @a: the first integer
+ * @b: the second int
+ * Return: the multipilication of the two inputs
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
