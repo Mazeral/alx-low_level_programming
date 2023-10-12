02756b2100b4fc89d0f7ed668408d3fc66f5f92c
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-12 22:23:01 +0200

    ALX nested loops and function part 2

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - the entry point
+ * Return: Always success 0
+ */
+
+int main(void)
+{
+int i = 1;
+for (; i <= 100; i++)
+{
+if(i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if(i % 5 == 0)
+{
+printf("Buzz ");
+}
+else 
+{
+printf("%d ", i);
+}
+}
+}
