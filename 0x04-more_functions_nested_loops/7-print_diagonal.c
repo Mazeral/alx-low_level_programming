3d0dfc2b16d38b6a418edd4075f1159f1af26c0d
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-12 21:44:40 +0200

    ALX nested loops and function part 2

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - a function that print backslashes
+ * @n: number of spaces
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (i > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar("\ ");
+}
+_putchar("\\");
+}
+}
+}
