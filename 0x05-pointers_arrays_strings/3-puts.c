3040489ba225b76c837c79300aaf7cfc12c6a758
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-16 16:53:24 +0200

    Pointers and arrays ALX

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * puts - a function to print a string
+ * _puts - a function to print a string
  * @str: the string
  * Return: Nothing, it's a void function
  */
@@ -9,7 +9,7 @@ void _puts(char *str)
 {
 int len = _strlen(str);
 int i = 0;
-while(str[i] != '\0')
+while (str[i] != '\0')
 {
 _putchar(str[i]);
 i++;
