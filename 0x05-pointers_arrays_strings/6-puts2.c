c7f181e52bae8f2f93798521ab5db55ada131b97
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-16 20:25:15 +0200

    Pointers and arrays ALX

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+/**
+ * puts2 - function that prints every other char of a string
+ * @str: the string
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if(i % 2 == 0)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
