60caade52e7b7b244435858c0fb43ce70de21fc6
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-16 16:19:46 +0200

    Pointers and arrays ALX

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * _puts - print a string
+ * @s the string
+ * Return: the string
+ */
+
+int _putchar(char *s)
+{
+int i = 0;
+int len = _strlen(s);
+for(;i < ; i++)
+write(1, &c, 1);
+}
