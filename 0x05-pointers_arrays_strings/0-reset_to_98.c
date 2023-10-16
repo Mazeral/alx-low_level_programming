0dec2695a212ebd19546ae00119cebeaf24af34c
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-16 14:36:20 +0200

    Pointers and arrays ALX

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98 - function that takes a pointer as a param and changed is associated value to 98
+ * @p*: pointer
+ * Return: Nothing
+ */
+void reset_to_98(int *p)
+{
+*p = 98;
+}
