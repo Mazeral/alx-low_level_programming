0fe90b91cef1493920ef14e50e7a47ce9e9abfe5
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-19 11:55:32 +0200

    Pointers, arrays and string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - Change all lower case letter to upper.
+ * @s: The string.
+ * Return: The string with upper cased letters.
+ */
+char *string_toupper(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] >= 97)
+{
+s[i] -= 32;
+}
+i++;
+}
+return (s);
+}
