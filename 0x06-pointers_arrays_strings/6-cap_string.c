43901b4236761f7a206a22582cf64de5538b590b
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-19 13:36:39 +0200

    Pointers, arrays and string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - a function to cap every word.
+ * @s: the string.
+ * Return: A string with the same words, captilized.
+ */
+
+char *cap_string(char *s)
+{
+int i = 0;
+int j;
+char list[] = {32, '\t', '\n', ',', ';',
+'.', '!', '?', '"', '(', ')', '{', '}'};
+while ((s[i] != '\0') && (s[i + 1] != '\0'))
+{
+if (i - 1 >= 0)
+{
+for (j = 0; list[j] != '\0'; j++)
+{
+if (s[i] == list[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+{
+s[i + 1] = s[i + 1] - 32;
+}
+}
+}
+i++;
+}
+return (s);
+}
