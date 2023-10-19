35dbb9671585e26b4665ef70a99c39786df7e5eb
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-19 14:15:01 +0200

    Pointers, arrays and string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,12 +10,9 @@ char list[] = "aAeEoOtTlL";
 char list2[] = "43071";
 int i = 0;
 int j = 0;
-
-// 0 1 2 3 4 5 6 7 8 9
-// 0 1 2 3 4
 while (list[i] != '\0')
 {
-for(j = 0; str[j] != '\0'; j++)
+for (j = 0; str[j] != '\0'; j++)
 {
 if (list[i] == str[j])
 {
