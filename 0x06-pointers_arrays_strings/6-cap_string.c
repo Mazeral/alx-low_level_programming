7415b4d1e69261ec3c6dd8023ef075e4775faf0f
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-19 13:52:43 +0200

    Pointers, arrays and string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
  * cap_string - a function to cap every word.
- * @s: the string.
+ * @str: the string.
  * Return: A string with the same words, captilized.
  */
 
-char *cap_string(char *str) {
+char *cap_string(char *str)
+{
 int i;
-int capitalize = 1;   
-for (i = 0; str[i] != '\0'; i++) {
+int capitalize = 1;
+for (i = 0; str[i] != '\0'; i++)
+{
 if (capitalize && isalpha(str[i]))
 {
 str[i] = toupper(str[i]);
 capitalize = 0;
-} else if (isspace(str[i]) || ispunct(str[i]))
+}
+else if (isspace(str[i]) || ispunct(str[i]))
 {
 capitalize = 1;
 }
 }
 return (str);
-;wa
 }
