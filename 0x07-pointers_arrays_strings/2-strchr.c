34df5001ea192e7cc0a6b15b1ec90f0ed6e6acd6
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-23 16:51:57 +0200

    Pointers, arrays and strings.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,10 +6,16 @@
  */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)	;
+int i = 0;
+while (s[i] != '\0')
+{
 if (s[i] == c)
+break;
+i++;
+}
+if (s[i] == c)
+{
 return (s + i);
-else
+}
 return (NULL);
 }
