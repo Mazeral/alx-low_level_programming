90d98fd85e00015af2985832b9f9557bebeb4dff
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-23 16:03:57 +0200

    Pointers, arrays and strings.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,9 @@
 /**
- * _memset() - fills the first n bytes of the memory area pointed to by s with the constant byte b
- * @s: the constant.
+ * _memset - fills the first n bytes of the memory area pointed to by s
+ * @s: The constant.
  * @n: Number of bytes.
  * @b: The byte.
+ * Return: the constant after the editing
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -11,5 +12,5 @@ for (; i < n && s[i] != '\0'; i++)
 {
 s[i] = b;
 }
-return s;
+return (s);
 }
