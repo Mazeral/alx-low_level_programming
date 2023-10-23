e99d7a61858bf821d0b334d4fd871a044cc77315
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-23 16:33:55 +0200

    Pointers, arrays and strings.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+/**
+ * _strchr -  a function that locates a character in a string.
+ * @s: The string
+ * @c: The character
+ * Return: the first occurance of char c in the string s
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+int j;
+for (; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+return (*(s + i));
+}
+}
+return (NULL);
+}
+
