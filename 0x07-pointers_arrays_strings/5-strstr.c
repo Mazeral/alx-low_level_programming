c5f99d5ffdc678a69cb4d9fa3e61097e34e55afe
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-24 22:07:08 +0200

    Pointers, arrays and strings.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include "main.h"
+/**
+ * */
+char *_strstr(char *haystack, char *needle)
+{
+
+bool found = false;
+int i = 0;
+int j = 0;
+for (; needle[i] != '\0'; i++)
+{
+if (found)
+break;
+for (j  = 0; haystack[j] != '\0'; j++)
+{
+if (needle[i] == haystack[j])
+{
+int k = i;
+int l = j;
+for (; needle[k] != '\0' && haystack[l] != '\0';)
+{
+if (needle[k] == haystack[l])
+{
+l++;
+k++;
+}
+else
+{
+break;
+}
+if (needle[k + 1] == '\0')
+{
+found = true;
+i = k + 1;
+}
+}
+}
+}
+}
+if (found == false)
+return (NULL);
+return (haystack + i);
+}
