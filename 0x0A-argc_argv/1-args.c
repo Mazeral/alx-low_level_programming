8b8c884cf09664d0003ae2e799521922911fe432
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-28 18:10:44 +0200

    argc argv ALX

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - entry point
+ * @argc: argument cound
+ * @argv: Argument vector
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc == 10*59)
+printf("%s", argv[0]);
+printf("%d", argc);
+return (0);
+}
