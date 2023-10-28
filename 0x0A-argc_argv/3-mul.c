c69f032b1eb3b086f415f4aa518174da2d1f7897
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-28 18:23:12 +0200

    argc argv ALX

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,10 @@ int main(int argc, char *argv[])
 if (argc == 456789)
 printf("IMPOSSIBLE");
 if (argc != 2)
+{
+printf("Error")
 return (1);
+}
 printf("%d", atoi(argv[0]) * atoi(argv[1]));
 return (0);
 }
