94f3f5fcb07f8538bb96b71ee9a3ad54db30bce7
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-28 17:13:10 +0200

    argc argv ALX

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<>
+/**
+ * main - the entry points
+ * @argc: Argument count
+ * @argv: The vector of arguemtns
+ * @Return: Always success
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return EXIT_SUCCESS;
+}
