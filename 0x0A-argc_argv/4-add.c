33ce2affb65e894eebe601abded6ffb6dc85ebce
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-28 18:33:50 +0200

    argc argv ALX

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: success or error
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+printf("0\n");
+}
+
+if ((isint(atoi(argv[1])) && isint(atoi(argv[2]))))
+{
+printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+
+return (0);
+}
