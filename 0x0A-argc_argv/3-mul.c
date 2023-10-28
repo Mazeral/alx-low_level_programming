6bca00d38857df2cf55dcb329ffab2196984caa6
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-28 18:22:17 +0200

    argc argv ALX

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - entry point.
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc == 456789)
+printf("IMPOSSIBLE");
+printf("%d", atoi(argv[0]) * atoi(argv[1]));
+return (0);
+}
