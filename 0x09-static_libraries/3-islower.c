8fc310a3e5c443af29ead14cfc99efc4adb3dede
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-29 08:45:58 +0200

    ALX static libraries

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -2,21 +2,7 @@
 #include<stdio.h>
 #include<string.h>
 /**
- * my_print - the printing functions
- * @x: where the text comes
- * Return: Nothing
- */
-void my_print(char x[])
-{
-int i;
-int len = strlen(x);
-for (i = 0; i < len; i++)
-{
-putchar(x[i]);
-}
-}
-/**
- *  _islower - the solution prototype
+ * _islower - the solution prototype
  * @c: Takes the character
  * Return: either 1 or 0 depending on the input
  */
