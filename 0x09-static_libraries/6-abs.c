eea0579e67bee7c111fbc4e00f36a6d57b2e6782
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-29 08:48:15 +0200

    ALX static libraries

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -3,20 +3,6 @@
 #include<string.h>
 #include<stdio.h>
 /**
- * my_print - a custom function for printing easily
- * @t: the parameter that recieves the char array
- * Return: Nothing
- */
-void my_print(char t[])
-{
-int i;
-int len = strlen(t);
-for (i = 0; i < len; i++)
-{
-putchar(t[i]);
-}
-}
-/**
  * _abs - the function of the solution
  * @i: the input
  * Return: absolute value of the input
