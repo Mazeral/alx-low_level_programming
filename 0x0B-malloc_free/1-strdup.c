e9fa51263598a02b84a2b5ed9a825d1b14b07b16
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-30 18:46:58 +0200

    Malloc free course

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,10 +10,12 @@
 char *_strdup(char *str)
 {
 char *dupstr;
+size_t size = strlen(str);
 if (str == NULL)
 return (NULL);
-dupstr = malloc(strlen(str));
+dupstr = malloc((size + 1) * sizeof(char));
 if (dupstr == NULL)
-return (NULL)
+return (NULL);
+strcpy(dupstr, str);
 return (dupstr);
 }
