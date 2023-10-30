6fd2842fb000af5a50c184a3537bd93731c49ac6
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-30 20:14:37 +0200

    Malloc free course

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+/**
+ * _str_concat - Concats 2 strings togther
+ * @s1: First string
+ * @s2: Second string
+ * Return: Null in case of failure or the concated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *concated;
+int i = 0;
+int j = 0;
+size_t size = 1 + strlen(s1) + strlen(s2);
+int size1 = strlen(s1);
+int size2 = strlen(s2);
+if (s1 == NULL && s2 == NULL)
+{
+return (NULL);
+}
+
+concated = malloc(size);
+if (concated)
+{
+return (NULL);
+}
+for (; s1[i] != '\0'; i++)
+{
+concated[i] = s1[i];
+}
+for (; s2[j] != '\0'; j++)
+{
+concated[i] = s2[j];
+i++;
+}
+concated[size] = '\0';
+return (concated);
+}
