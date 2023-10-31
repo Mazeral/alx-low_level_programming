6d00217bbeebbb018503189a3376020238c70374
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-31 11:09:37 +0200

    Malloc free course

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,30 +17,12 @@ free(grid);
 return (NULL);
 }
 for (i = 0; i < height; i++)
-{
 grid[i] = malloc(width * sizeof(int *));
-if (grid[i] == NULL)
-{
-free(grid);
-return (NULL);
-}
-}
 
 for (i = 0; i < height; i++)
 for (; j < width; j++)
 grid[i][j] = 0;
 
-for (; j < width; j++)
-{
-if (grid[j] == NULL)
-{
-for (i = 0; i < height; i++)
-{
-for (j = 0; j < width; j++)
-free(grid[i][j]);
-}
-}
-}
 if (grid == NULL)
 {
 free(grid);
