375b30e9f456dbec473f21da711820a7c6be166d
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-10-31 08:06:42 +0200

    Malloc free course

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,28 +2,6 @@
 #include <stdlib.h>
 #include<string.h>
 /**
- * check_null -  a function to check the null values in the parameters
- * @s1: The first param
- * @s2: The second param
- * Return: Depending on the values
- */
-char *check_null(char *s1, char *s2)
-{
-if (s1 == NULL && s2 == NULL)
-{
-return (NULL);
-}
-else if (s1 != NULL && s2 == NULL)
-{
-return (s1);
-}
-else if (s1 == NULL && s2 != NULL)
-{
-return (s2);
-}
-return ("");
-}
-/**
  * str_concat - Concats 2 strings togther
  * @s1: First string
  * @s2: Second string
@@ -35,21 +13,16 @@ char *concated;
 int i = 0;
 int j = 0;
 size_t size;
-if (s1 == NULL || s2 == NULL)
-{
-if (check_null(s1, s2) != NULL)
-{
-char *check = malloc(strlen(check_null(s1, s2)));
-check = check_null(s1, s2);
-return (check);
-}
-else
-{
-return (NULL);
-}
-}
+char *str1 = s1;
+char *str2 = s2;
+if (str1 == NULL)
+str1 = "";
+if (str2 == NULL)
+str2 = "";
 size = 1 + strlen(s1) + strlen(s2);
 concated = malloc(size);
+if (concated == NULL)
+return (NULL);
 for (; s1[i] != '\0'; i++)
 {
 concated[i] = s1[i];
