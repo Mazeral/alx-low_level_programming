979d466c9731448b54ff8a424cafce9890e187e7
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-02 09:47:21 +0200

    More malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 
 /**
- * string_nconcat - a function that concats a str and the n chars of the second str
+ * string_nconcat - a function that concats
  * @s1: The first string
  * @s2: The second string
  * @n: The letters of the second string
@@ -13,13 +13,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 int j = 0;
 int i = 0;
 unsigned int size = strlen(s1) + n;
-char s* = malloc(strlen(s1) + n + 1);
+char *s = malloc(strlen(s1) + n + 1);
 if (s == NULL)
 return (NULL);
 for (; i < strlen(s1); i++)
 s[i] = s1[i];
 
-for(; j < n; j++)
+for (; j < n; j++)
 {
 s[i] = s2[j];
 }
