36b9691ef79642b0122cfcd6ed575ea79cf5793c
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-02 16:58:42 +0200

    More malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include<stdlib.h>
 #include<string.h>
 #include<stdio.h>
-
 /**
  * string_nconcat - a function that concats
  * @s1: The first string
@@ -19,21 +18,17 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-
 size = strlen(s1) + strlen(s2) + 1;
 s = malloc(sizeof(char) * (size + 1));
 if (s == NULL)
 return (NULL);
-
 for (i = 0; s1[i] != '\0'; i++)
 s[i] = s1[i];
-
 for (j = 0; j < n && s2[j] != '\0'; j++)
 {
 s[i] = s2[j];
 i++;
 }
-
 s[i] = '\0';
 if(s == NULL)
 return (NULL);
