cd3a397bd8cf1cf50111ac4782249c1f5fb1d076
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-02 09:37:17 +0200

    More malloc free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * _calloc - a function which mallocs but with zeros
+ * @nmemb: Numbers of places.
+ * @size: The size of the pointer
+ * Return: A void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int i = 0;
+void *p = malloc(nmemb * size);
+for (; i < nmemb; i++)
+p[i] = 0;
+}
