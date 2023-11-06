705654e15982b7426117a2abf1e0bf0d3d2533f1
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-06 21:52:24 +0200

    ALX structures typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "dog.h"
-
+/**
+ * free_dog - frees the memory of the dog
+ * @d: The dog struct
+ * Return: Nothing
+ */
 void free_dog(dog_t *d)
 {
 if (d)
