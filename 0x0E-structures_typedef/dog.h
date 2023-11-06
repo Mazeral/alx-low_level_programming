1c6c80125ff0c4de5402c33eb5c90ff355dd6076
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-06 20:50:34 +0200

    ALX structures typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - A struct for a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner name
+ * Description: Simple struct for the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
