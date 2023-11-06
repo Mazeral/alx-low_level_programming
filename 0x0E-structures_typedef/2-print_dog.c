b55151adf4b7618397555c5976976386eb2f4d1b
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-06 21:36:45 +0200

    ALX structures typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,12 +14,9 @@ if (d->name != NULL)
 printf("Name: %s", d->name);
 else
 printf("Name: (nil)");
-if (d->age != NULL)
-printf("Age: %d", d->age);
-else
-printf("Age: (nil)");
+printf("Age: %.2f", d->age);
 if (d->owner != NULL)
-printf("Owner: %s\n", s->owner);
+printf("Owner: %s\n", d->owner);
 else
 printf("Owner: (nil)\n");
 }
