a23986a14d30ec046a65e224b2cf7122ddc778e7
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-06 21:15:27 +0200

    ALX structures typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints the dog
+ * @d: Details about the dog
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+printf("Name: %s", d->name);
+printf("Age: %d", d->age);
+printf("Owner: %s", s->owner);
+}
