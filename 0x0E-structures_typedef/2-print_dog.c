58936d6a4726c1564c2bf078293824ce4fdc0e11
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-06 21:29:00 +0200

    ALX structures typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,16 @@
  */
 void print_dog(struct dog *d)
 {
+if (d)
+{
+if (d->name != NULL)
 printf("Name: %s", d->name);
+else printf("Name: (nil)");
+if (d->age != NULL)
 printf("Age: %d", d->age);
+else printf("Age: (nil)");
+if (d->owner != NULL)
 printf("Owner: %s", s->owner);
+else printf("Owner: (nil)");
+}
 }
