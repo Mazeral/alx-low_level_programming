5874f787c957ca7965865539067ba40ee7cafbb6
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-07 17:49:58 +0200

    ALX structures typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,6 @@ int i;
 char *owner_name;
 if (name == NULL || owner == NULL)
 return (NULL);
-// make sure to make the pointer of char as long as the sttring, then go through for loop, after the for loop check for any errors
 *dog_name = malloc(strlen(name) + 1);
 *owner = malloc(strlen(owner) + 1);
 for(i = 0; name[i] != '\0'; i++)
