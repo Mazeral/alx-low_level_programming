1515fda6fe20eb915a1ceaf1bc3614b8f6d5dd80
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-07 18:09:01 +0200

    ALX structures typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,16 +23,16 @@ dog = malloc(sizeof(*dog));
 
 dog_name = (char *) malloc(strlen(name) * sizeof(char) + 1);
 owner = (char *) malloc(strlen(owner) * sizeof(char) + 1);
-for(i = 0; name[i] != '\0'; i++)
+for (i = 0; name[i] != '\0'; i++)
 dog_name[i] = name[i];
 
-for(i = 0; owner[i] != '\0'; i++)
+for (i = 0; owner[i] != '\0'; i++)
 owner_name[i] = owner[i];
 
-for(i = 0; owner_name[i] != '\0'; i++)
+for (i = 0; owner_name[i] != '\0'; i++)
 dog->owner[i] = owner_name[i];
 
-for(i = 0; dog_name[i] != '\0'; i++)
+for (i = 0; dog_name[i] != '\0'; i++)
 dog->name[i] = dog_name[i];
 dog->age = age;
 return (dog);
