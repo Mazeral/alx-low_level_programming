c62a0e2980434358405748932424698f66abb709
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-08 07:53:35 +0200

    ALX structures typedef

---
+++ 0x0E-structures_typedef/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - similar to strcpy, but with an n
+ * @dest: the array to be changed
+ * @src: the change
+ * @n :The number of letters to be changed
+ * Return: dest after change
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while (src[j] != '\0' && j < n && dest[i] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,8 +24,8 @@ new_dog = malloc(sizeof(*new_dog));
 dog_name = malloc((strlen(name) + 1) * sizeof(char));
 owner_name = malloc((strlen(owner) + 1) * sizeof(char));
 
-strcpy(dog_name, name);
-strcpy(owner_name, owner);
+_strcpy(dog_name, name);
+_strcpy(owner_name, owner);
 
 new_dog->name = dog_name;
 new_dog->owner = owner_name;
