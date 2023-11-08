6203637a2d0b9c7f6f5c36a84bb5c30c0b0c9e3b
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-08 20:16:46 +0200

    Fucntion pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,54 +1,54 @@
 /**
  * opp_add - adds 2 numbers
- * @x: First number
- * @y: Second number
+ * @a: First number
+ * @b: Second number
  * Return: The result
  */
-int op_add(int x, int y)
+int op_add(int a, int b)
 {
-return (x + y);
+return (a + b);
 }
 
 /**
  * op_sub - sub 2 numbers
- * @x: First number
- * @y: Second number
+ * @a: First number
+ * @b: Second number
  * Return: The result
  */
-int op_sub(int x, int y)
+int op_sub(int a, int b)
 {
-return (x - y);
+return (a - b);
 }
 
 /**
  * op_mod - mods 2 numbers
- * @x: First number
- * @y: Second number
+ * @a: First number
+ * @b: Second number
  * Return: The result
  */
-int op_mod(int x, int y)
+int op_mod(int a, int b)
 {
-return (x % y);
+return (a % b);
 }
 
 /**
- * op_mul - multiply 2 numbers
- * @x: First number
- * @y: Second number
+ * op_mul - multiplb 2 numbers
+ * @a: First number
+ * @b: Second number
  * Return: The result
  */
-int op_mul(int x, int y)
+int op_mul(int a, int b)
 {
-return (x * y);
+return (a * b);
 }
 
 /**
  * op_div - divides 2 numbers
- * @x: First number
- * @y: Second number
+ * @a: First number
+ * @b: Second number
  * Return: The result
  */
-int op_div(int x, int y)
+int op_div(int a, int b)
 {
-return (x / y);
+return (a / b);
 }
