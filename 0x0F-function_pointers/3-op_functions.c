fc9316e3b98356e16e381a4629bbfd7c5eede67a
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-08 20:14:53 +0200

    Fucntion pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * opp_add - adds 2 numbers
+ * @x: First number
+ * @y: Second number
+ * Return: The result
+ */
+int op_add(int x, int y)
+{
+return (x + y);
+}
+
+/**
+ * op_sub - sub 2 numbers
+ * @x: First number
+ * @y: Second number
+ * Return: The result
+ */
+int op_sub(int x, int y)
+{
+return (x - y);
+}
+
+/**
+ * op_mod - mods 2 numbers
+ * @x: First number
+ * @y: Second number
+ * Return: The result
+ */
+int op_mod(int x, int y)
+{
+return (x % y);
+}
+
+/**
+ * op_mul - multiply 2 numbers
+ * @x: First number
+ * @y: Second number
+ * Return: The result
+ */
+int op_mul(int x, int y)
+{
+return (x * y);
+}
+
+/**
+ * op_div - divides 2 numbers
+ * @x: First number
+ * @y: Second number
+ * Return: The result
+ */
+int op_div(int x, int y)
+{
+return (x / y);
+}
