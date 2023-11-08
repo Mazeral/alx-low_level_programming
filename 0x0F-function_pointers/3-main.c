28ef79ac3aa3fcdf18dfbd2a8c17388dd8c9007b
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-08 19:52:43 +0200

    Fucntion pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,23 +10,21 @@
 
 int main(int argc, char *argv[])
 {
-/*if the number of arguments is wrong, print Error, followed by a new line, and exit with the status 98
-if the operator is none of the above, print Error, followed by a new line, and exit with the status 99
-if the user tries to divide (/ or %) by 0, print Error, followed by a new line, and exit with the status 100*/
 if (argc > 3)
 {
 printf("Error\n");
 exit(98);
 }
-if (argv[1] != '/' || argv[1] != '+' || argv[1] != '-' || argv[1] != '*' || argv[1] != '%')
+if (argv[1] != '/' || argv[1] != '+' ||
+argv[1] != '-' || argv[1] != '*' || argv[1] != '%')
 {
 printf("Error\n");
-exit (99);
+exit(99);
 }
 if (argv[2] == '0')
 {
 printf("Error\n");
-exit (100);
+exit(100);
 }
 return (0);
 }
