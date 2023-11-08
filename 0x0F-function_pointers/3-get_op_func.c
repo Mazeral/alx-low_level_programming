d7bb11f3b14b4109cf8820e503c9b5e0871d83a2
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-08 19:31:16 +0200

    Fucntion pointers

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include "calc.h"
+/**
+ * get_op_func - a function that gets the right operation
+ * @c: The operation
+ * @int: The first int
+ * @int: The second int
+ * Return: error or success
+ */
+int (*get_op_func(char *s))(int, int)
+{
+
+}
