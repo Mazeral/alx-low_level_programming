255244ae193e5d0c22316590c94cf383598a721b
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-08 19:45:12 +0200

    Fucntion pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * @s: The operation
  * Return: error or success
  */
-int (*get_op_func(char *s))(int, int)
+int (*get_op_func(char *s))(int x, int y)
 {
 op_t ops[] = {
 {"+", op_add},
@@ -15,5 +15,31 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL}
 };
+
 int i;
+if (s[1] != '\0')
+{
+printf("Error\n");
+exit(98);
+}
+
+while (i < 5)
+{
+if (s == ops[i][0])
+{
+if (ops[i][0] == '/' || ops[i][0] == '%')
+{
+if (y == 0)
+{
+printf("Error\n");
+exit(100);
+}
+}
+return (ops[i][1](int, int));
+}
+i++;
+}
+
+printf("Error\n");
+exit(99);
 }
