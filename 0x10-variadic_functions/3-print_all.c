eb9c864782a2caad2a035b980267f43d7c5b6d01
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-09 13:39:31 +0200

    Fucntion pointers

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include<stdarg.h>
+#include<stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - a function that prints everything
+ * @format: The type of the arg
+ * Return: Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+
+}
