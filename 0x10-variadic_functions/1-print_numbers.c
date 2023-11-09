1c846af0953e5e510e9762732b0b8972dd299f8f
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-09 14:11:42 +0200

    Fucntion pointers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "variadic_functions.h"
 #include<stdlib.h>
 #include<stdarg.h>
 /**
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,4 +1,5 @@
 #include<string.h>
+#include "variadic_functions.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdarg.h>
