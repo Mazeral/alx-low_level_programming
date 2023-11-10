26f5d9513948e352c8ecafd1552f54be70ce030f
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-10 13:50:07 +0200

    Fucntion pointers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,9 +10,9 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+unsigned int i = 0;
 va_list args;
 va_start(args, n);
-unsigned int i = 0;
 for (; i < n; i++)
 {
 if (i + 1 == n)
