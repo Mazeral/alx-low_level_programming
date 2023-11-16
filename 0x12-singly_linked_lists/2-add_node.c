31e998bea920caff10aee055e75a0995d12050b4
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-16 17:55:09 +0200

    Edited 2-addnode.

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * add_node - adds a node to the beginning of the linked list
+ * @head: The head of the linked list
+ * @str: The value of in the lnked list
+ * Return: A linked list node
+ */
+
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *next = malloc(sizeof(list_t));
+next->str = strdup(str);
+next->len = strlen(str);
+
+head->next = next;
+return (next);
+}
