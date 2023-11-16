8196895ab45e2998fc615afaabeed010fd01a938
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-16 17:04:26 +0200

    Edited the print_list function.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,11 +9,20 @@
 size_t print_list(const list_t *h)
 {
 int i = 0;
+/*
+ * Creating a pointer that goes though all the elements*/
 list_t *current = &h;
-while (h->next != NULL)
+/*
+ * Atleast, there WILL be one NODE in the list
+ */
+do
 {
-current = h->next;
+if (current->str != NULL)
+printf("%s%d", current->str, current->len);
+else
+printf("(nil), %d", current->len);
+current = current->next;
 i++;
-}
+} while (current->next != NULL)
 return (i);
 }
