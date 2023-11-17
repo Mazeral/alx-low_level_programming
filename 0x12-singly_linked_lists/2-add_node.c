75e62699f7ef106e51e157490db1829335ab7c8c
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-17 07:46:05 +0200

    trying to fix add node erroes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,15 +13,19 @@ list_t *add_node(list_t **head, const char *str)
 int len = 0;
 list_t *node = malloc(sizeof(list_t));
 if (node == NULL)
+{
+free (node);
 return (NULL);
+}
 while (str[len])
 len++;
 node->str = malloc(sizeof(str));
-strcpy(node->str, str);
 if (node->str == NULL)
 {
 free(node);
+free(len);
 }
+strcpy(node->str, str);
 node->len = len;
 node->next = (*head);
 if (node)
