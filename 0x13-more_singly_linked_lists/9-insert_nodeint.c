8b69f5e14d1e4f140c21b94cf6b1d94eaa654796
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-20 07:57:30 +0200

    Created insert_nodeint file and function.

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * listint_t - adds a node at a given positiion in the linked list
+ * @head: the head of the linked list
+ * @idx: The position of the new element
+ * @n: The value in the node
+ * Return: The address of the node in the linked list
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+unsigned int index = 0;
+listint_t **current;
+listint_t **alt;
+listint_t *newNode;
+if (!head)
+return (NULL);
+
+newNode->n = n;
+current = head;
+while (current && index < idx)
+{
+if ((*head)->next)
+current = (*head)->next;
+else
+{
+return (NULL);
+}
+index++;
+}
+if (!newNode)
+return (NULL);
+
+alt = (*current)->next;
+(*current)->next = newNode;
+newNode->next = *alt;
+return (newNode);
+}
