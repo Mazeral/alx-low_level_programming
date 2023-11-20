c1d8d2fd228f60b66c0511a1f66c6e2184fb01fb
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-20 07:39:13 +0200

    Created sum_listint function and file.

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Sums all the data in the linked liste
+ * @head: the head of the linked list
+ * Return: the sum of the data in the linked list
+ */
+
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+listint_t *current = head;
+if (!head)
+return (sum);
+
+while (current)
+{
+sum+=current->n;
+current = current->next;
+}
+return (sum);
+}
