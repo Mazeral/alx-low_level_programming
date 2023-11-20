89a5deeb749af4e8264ed2dfd9c93c37b28c391f
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-20 06:22:44 +0200

    Created listint_len file and function

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - return the number of nodes in the linked list
+ * @h: The head of the node
+ * Return: i, the number of the nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+int i = 0;
+listint_t **node;
+if (!h)
+return (i);
+
+node = &h;
+while ((*node)->next)
+{
+node = (*node)->next;
+i++;
+}
+return (i);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,4 +16,5 @@ typedef struct listint_s
 int n;
 struct listint_s *next;
 } listint_t;
+size_t listint_len(const listint_t *h);
 #endif
