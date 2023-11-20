fa4074fe6fd21a2f39dac9b6044aeb0ccf5b0e2e
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-20 06:27:06 +0200

    Created add_nodeint file and function

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - addes a node at the beginning of the linked list
+ * @head: The head of the linked list
+ * @n: The data in the new node
+ * Return: NULL if failed, the address of the new element if success
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+if (!head)
+return (NULL)
+
+listint_t *newNode;
+newNode.n->n;
+newNode.next->NULL;
+
+(*head)->next = node;
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,4 +17,5 @@ int n;
 struct listint_s *next;
 } listint_t;
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
