eaea076aaba35c02c0a7aa2a4142a7a00510a86d
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-21 13:15:42 +0200

    Fixing bugs on add_nodeint_end function.

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,10 +9,12 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+listint_t endNode = malloc(sizeof(listint_t));
+listint_t node = malloc(sizeof(listint_t));
 if (!head)
 return (NULL)
 
-listint_t **node = head;
+listint_t *node = head;
 listint_t *endNode;
 endNode->n = n;
 endNode->next = NULL;
