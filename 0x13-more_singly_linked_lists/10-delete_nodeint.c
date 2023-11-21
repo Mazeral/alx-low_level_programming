14bee2213879960692638908fb8e6618e70e22be
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-21 19:58:27 +0200

    Fixing errors in insert node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,32 +8,34 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-unsigned int i;
-listint_t *temp, *new;
+listint_t *current;
+listint_t *alt1 = malloc(sizeof(listint_t));
+listint_t *alt2 = malloc(sizeof(listint_t));
+unsigned int i = 0;
+if (!head)
+return (-1);
 
-if (head == NULL)
-return (NULL);
-if (idx != 0)
+current = *head;
+while (current && i < index - 1)
 {
-temp = *head;
-for (i = 0; i < idx - 1 && temp != NULL; i++)
+current = current->next;
+i++;
+}
+if (!current || i == index || !current->next)
+return (-1);
+
+/* current is the element before the node to be deleted. */
+/* alt1 is the node which we want to delete. */
+/* alt2 is the node that exist if there's a node after alt1. */
+alt1 = current->next;
+if (alt1->next)
 {
-temp = temp->next;
+alt2 = at1->next;
+current->next = alt2;
+free(alt1->next);
+free(alt1);
 }
-if (temp == NULL)
-return (NULL);
-}
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-new->n = n;
-if (idx == 0)
-{
-new->next = *head;
-*head = new;
-return (new);
-}
-new->next = temp->next;
-temp->next = new;
-return (new);
+current->next = NULL;
+free(alt1->next);
+free(alt1);
 }
