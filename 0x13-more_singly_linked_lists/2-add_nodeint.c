9871a1e977dfc67f52d2d5e5f006319ca49e52cd
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-21 10:44:18 +0200

    Fixing bugs on add_nodeint function.

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,15 +10,10 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *newNode = malloc(sizeof(listint_t));
+if (!newNode)
+return (NULL);
 newNode->n = n;
-newNode->next = NULL;
-
-if ((*head)->next)
-{
-newNode->next = (*head)->next;
-(*head)->next = newNode;
-}
-else
-(*head)->next = newNode;
+newNode->next = *head;
+*head = newNode
 return (newNode);
 }
