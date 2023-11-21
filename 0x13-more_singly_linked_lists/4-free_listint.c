24f7e44a822cb3f6d60f086ba41eb679d3ba2ca5
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-21 16:49:30 +0200

    Fixing bugs on 6

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,16 +6,16 @@
  * Return: Nothing
  */
 
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
-listint_t *current1 = head;
-listint_t *current2 = head;
-if (head == NULL)
-return;
-while (current1 != NULL)
+listint_t *tmp;
+
+if (head)
 {
-current1 = head->next;
-free(current2);
-current2 = current1;
+while (*head != 0)
+{
+tmp = *head;
+*head = tmp->next;
+free(tmp);
 }
 }
