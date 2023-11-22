4f37a3b3126c25c0b99dbe01b6b83988b32cbf47
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-22 06:54:16 +0200

    Fixed bug on get_nodeint.

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *current = head;
-if (!head)
-return (NULL);
-
-while (current && i < index)
+while (current != NULL)
 {
+if (i == index)
+return (current)
+else
+{
+i++;
 current = current->next;
 }
-return (current);
+}
 }
