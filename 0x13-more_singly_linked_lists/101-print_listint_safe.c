683bbeb17d86ff3cf400161c22163118d985e4a5
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-22 09:29:13 +0200

    Created the print_listint_safe file and function.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,7 +12,7 @@ size_t print_listint_safe(const listint_t *head)
 size_t i = 0;
 while (head)
 {
-printf("[%p] %d\n",head, head->n);
+printf("[%p] %d\n",&head, head->n);
 head = head->next;
 i++;
 }
