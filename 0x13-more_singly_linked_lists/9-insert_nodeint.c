5f471c237d3e402aec37d2fa146ead4d95b7100c
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-22 07:35:44 +0200

    Fixed bug on insert_node.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,6 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i;
-unsigned int index = 0;
 listint_t *current = malloc(sizeof(listint_t));
 listint_t *alt = malloc(sizeof(listint_t));
 listint_t *newNode = malloc(sizeof(listint_t));
