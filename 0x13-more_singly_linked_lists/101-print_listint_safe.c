d5de83fb9bc4747bc3b4caac6a4cb3b384ba4a71
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-22 09:24:22 +0200

    Created the print_listint_safe file and function.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints the contents of the linked list
+ * @head: The head of the linked list
+ * Return: The length of the linked list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+
+/* [0x1b500f0] 1024 */
+size_t i = 0;
+while (h)
+{
+printf("[%p] %d\n",h, h->n);
+h = h->next;
+i++;
+}
+return (i);
+}
