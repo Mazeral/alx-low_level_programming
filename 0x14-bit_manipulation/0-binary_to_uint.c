6cbc567d7e35622f5802ef1bd283d9fc40e5fb96
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-23 16:27:22 +0200

    Fixed bugs in binary_to_uint.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,9 +9,9 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sum = 0;
-	int i, j, pow;
+	int i = strlen(b) - 1, j, pow;
 
-	for (i = strlen(b), i > 0, i--)
+	for (i = strlen(b); i >= 0; i--)
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
@@ -27,6 +27,6 @@ unsigned int binary_to_uint(const char *b)
 		{
 			return (0);
 		}
-}
+	}
 	return (sum);
 }
