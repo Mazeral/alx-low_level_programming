6ac4905700523533b74675b9da84ad2d3389d6ed
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-23 14:22:57 +0200

    Created get_bit file. Fixing errors in the main.h file.

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the value of the binary in a position
+ * @n: The number
+ * @index: the index of the binary number in the binary representation
+ * Return: The value of the binary in the index in the binary rep of the int
+ */
+int get_bit(unsigned long int n, unsigned int index);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<math.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-
+int get_bit(unsigned long int n, unsigned int index);
 
 
 
