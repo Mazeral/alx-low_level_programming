d36cfbcd22bdf4ccea801ff229fa21f015cb522c
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-23 20:55:24 +0200

    Trying to fix get_bit function.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sum = 0;
-	int i = strlen(b) - 1, j, pow;
-
+	int a = strlen(b) - 1, j, pow, i = 0;
 	if (b == NULL)
 		return (0);
 
-	for (; i >= 0; i--)
+	for (; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
 			if (b[i] == '1')
 			{
 				pow = 1;
-				for (j = 1; j < i; j++)
+				for (j = 0; j < a; j++)
 				pow *= 2;
 				sum += pow;
 			}
+		a--;
 		}
 		else
 		{
