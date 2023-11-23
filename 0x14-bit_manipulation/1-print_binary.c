54679e8d1e72504d186b148a813beda6e4b7ca98
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-23 16:55:19 +0200

    Fixed some error found in print_binary. added a function to reverse a string. added 1-main.c to test the function.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,60 @@
 #include "main.h"
 
+void reverse(char str[]);
+
 /**
  * print_binary - prints the binary representation of the number
  * @n: The number
  * Return: Nothing
  */
+
 void print_binary(unsigned long int n)
 {
-while (n != 0)
-{
-if (n & 1)
-printf("1");
-else
-printf("0");
-n = n >> 1;
+	unsigned int malloced = 1;
+	unsigned long int x = n;
+	char *number;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	while (x != 0)
+	{
+	malloced++;
+	x = x / 2;
+	}
+	number = malloc(malloced);
+	while (n != 0)
+	{
+		if (n & 1)
+		strcat(number, "1");
+		else
+		strcat(number, "0");
+		n = n >> 1;
+	}
+	reverse(number);
+	printf("%s", number);
 }
+
+
+/**
+ * reverse - a function to reverse a string
+ * @str: The string
+ * Return: Nothing
+ */
+
+void reverse(char str[])
+{
+	int i = 0, j = strlen(str) - 1;
+	char temp;
+
+	while (i < j)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i++;
+		j--;
+	}
 }
