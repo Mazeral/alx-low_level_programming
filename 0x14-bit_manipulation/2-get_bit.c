6ab6e70dce0fb5199e282a4cc4959f74bd3f16e5
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-23 19:28:13 +0200

    Created and fix get_bit function created 2-main to test get_bin function.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,4 +6,16 @@
  * @index: the index of the binary number in the binary representation
  * Return: The value of the binary in the index in the binary rep of the int
  */
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int  x;
+
+	if (index > 32)
+		return (-1);
+	
+	x = n >> (index - 1);
+	if ((x >> 1 && 1) == 1)
+		return (1);
+
+	return (0);
+}
