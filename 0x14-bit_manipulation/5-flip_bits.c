57274fd653d970f56b97a960cad83a9bfe659925
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-23 20:36:47 +0200

    Created flip_bits.c file and function Created 5-main.c file.

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - return the number of bits you need to flip in order to get from one number to other
+ * @n: the number
+ * @m: The number to reach
+ * Return: Number of flips
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^ m, flips = 0;
+	while (x != 0)
+	{
+		if ((x & 1) == 1)
+		flips++;
+		x = x >> 1;
+	}
+	return (flips);
+}
