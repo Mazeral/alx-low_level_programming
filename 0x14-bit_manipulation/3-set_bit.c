8dd857eb90dbcbc66a7e92eca0462008c7bf24df
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-23 19:42:23 +0200

    Created set_bit file and function. Created 3-main.c to test the function.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - adds a bit in the index
+ * @n: The number to put the bit in
+ * @index: The index of the bit
+ * Return: The number after the edit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 1, i = 0;
+
+	if (index > 32)
+		return (-1);
+	
+	for (; i < index; i++)
+		bit *= 2;
+
+	*n = bit | *n;
+	return (1);
+}
