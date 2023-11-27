5b40be531d4eb09678233bb471e74acc9bc23c72
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-27 18:16:41 +0200

    Created 0-main.c Created 0-read_textfile.c Created main.h file Created README.md .

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * read_textfile - read a text file and prints it to the POSIX std output
+ * @filename: The name of ther file
+ * @letters: The letters in the file.
+ * Return: 0 or else
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *f;
+	char chr;
+	size_t counter = 0;
+
+	if (filename == NULL)
+		return (0);
+
+	f = fopen(filename, "r");
+	while (fscanf(f, "%c", &chr) == 1 && counter < letters)
+	{
+		printf("%c", chr);
+		counter++;
+	}
+
+	fclose(f);
+	return (counter);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+# ALX Software engineering course
+0x15 file in put and output lesson.
