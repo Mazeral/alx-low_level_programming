9c63614ee0a7239391968d12c8c15823ca38cd55
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-28 19:45:57 +0200

    Fixed cp.c file and function.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,19 +1,15 @@
 #include "main.h"
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
- * copy - a function that copies the contents from one file and paste it in another
- * @src: The source of the material to be copied.
- * @dest: The place where the material will be pasted
- * Return: 1 if sucess, 0 if failure
+ * main - entry point
+ * @argc: The count of the arguments
+ * @argv: The vector containing every argument
+ * Return: 0
  */
-
-ssize_t copy(int src, int dest)
-{
-	/* In this situation, it's better to use FILE *f rather int file_descriptor,
-	 * Since we can use funcitons like fscanf to save the text. */
-}
-
 int main(int argc, char *argv[])
 {
 	int file_descriptor_src = open(argv[1], O_RDONLY);
@@ -22,37 +18,35 @@ int main(int argc, char *argv[])
 	int write_checker;
 	char buffer[4096];
 	size_t file_size = 0;
-	size_t bytes_read;
+	ssize_t bytes_read;
 
 	if (file_descriptor_src == -1)
 	{
-		pritnf("Error: Can't read from file %s\n", src);
+		printf("Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_descriptor_dest == -1)
 	{
-		printf("Error: Can't write to file %s\n", dest);
+		printf("Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-	/* Reading the src file and putting all
-	 * it's content in the content char pointer. */
-	while (bytes_read = read(file_descriptor_src, buffer, sizeof(buffer)) > 0)
+	while ((bytes_read = read(file_descriptor_src, buffer, sizeof(buffer))) > 0)
 	{
 		file_size += bytes_read;
 	}
-
-	/* Malloc the contents. */
 	content = malloc(file_size);
-
-	/* Make the content have the actual contents of the src file. */
-	content = read(file_descriptor_src, content, file_size);
-
-	/* Writing the content in the dest file. */
+	lseek(file_descriptor_src, 0, SEEK_SET);
+	if (read(file_descriptor_src, content, file_size) == -1)
+	{
+		printf("Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	write_checker = write(file_descriptor_dest, content, file_size);
 	if (write_checker == -1)
 	{
 		printf("Error: Can't write to file %s\n", argv[2]);
 	}
-
+	close(file_descriptor_src);
+	close(file_descriptor_dest);
 	return (0);
 }
