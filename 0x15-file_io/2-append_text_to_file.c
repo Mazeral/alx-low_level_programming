9a8d94d2ebb725db8a772beb6242884e40a7cbf4
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-28 08:03:51 +0200

    Created append_text file and function. Created 2-main file and function.

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * append_text_to_file - appends a text to the end of the file
+ * @filename: The name of the file.
+ * @text_content: The content to be appeneded.
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_descriptor = open(filename, O_WRONLY | O_CREAT | O_APPEND);
+	int write_check;
+	if (file_descriptor == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+	write_check = write(file_descriptor, text_content, strlen(text_content));
+	if (write_check == -1)
+		return (-1);
+	close(file_descriptor);
+	return (1);
+}
