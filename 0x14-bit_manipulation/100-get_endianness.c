c4b60ead9d61327df748058527fd27e7643d0248
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-29 07:11:38 +0200

    Created big_endiannes function and file.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks if the system is big endianness or small
+ * Return: 0 if big, 1 if small
+ */
+int get_endianness(void)
+{
+	int sev = 7;
+
+	if ((sev >> 1) & 1 == 1)
+		return (1);
+	else
+		return (0);
+}
